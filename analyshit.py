25d87b1493ab3c5897e7cc520db3bfe27bfe096f
nosd <nosd@zhaw.ch>
2020-04-25 12:41:15 +0200

    adding argparse

---
+++ analyshit.py
@@ -0,0 +1,29 @@
+"""
+Analyshit
+
+Analyzing the defecational behavior over the span of one year.
+
+Diagrams and Outputs:
+- Average values (consistency, size)
+- Counts (glück, ninja, etc.)
+- Heat Map (consistency, size)
+- Average values on Weekdays (consistency, size)
+
+
+"""
+
+import argparse
+
+def parse_input_arguments():
+	parser = argparse.ArgumentParser(description="Analyzing the defecational behavior over the span of one year.")
+	parser.add_argument("-f", "--file", help="Specify the desired file.")
+	args = parser.parse_args()
+	return args
+
+
+def main():
+	args = parse_input_arguments()
+
+
+if __name__ == "__main__":
+	main()
