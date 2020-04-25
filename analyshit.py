0d1a22f98f6941f5be920c69766a09dfce3de5ed
nosd <nosd@zhaw.ch>
2020-04-25 22:00:49 +0200

    added dash example page

--- analyshit.py
+++ analyshit.py
@@ -12,9 +12,40 @@ Diagrams and Outputs:
 
 """
 
+# General
 import argparse
 import os
 import logging
+
+# Dash
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+	html.H1(children='Hello Dash'),
+
+	html.Div(children='''
+		Dash: A web application framework for Python.
+	'''),
+
+	dcc.Graph(
+		id='example-graph',
+		figure={
+			'data': [
+				{'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+				{'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+			],
+			'layout': {
+				'title': 'Mario Data Visualization'
+			}
+		}
+	)
+])
 
 def parse_input_arguments():
 	parser = argparse.ArgumentParser(description="Analyzing the defecational behavior over the span of one year.")
@@ -51,6 +82,8 @@ def main():
 	else:
 		filename = get_newest_filename()
 		print(filename)
+
+	app.run_server(debug=True)
 
 
 
