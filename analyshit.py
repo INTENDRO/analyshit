b632bbf1179e27abc31d6da8096083fadd57cb2e
nosd <nosd@zhaw.ch>
2020-04-25 13:10:05 +0200

    getting filename

--- analyshit.py
+++ analyshit.py
@@ -13,6 +13,7 @@ Diagrams and Outputs:
 """
 
 import argparse
+import os
 
 def parse_input_arguments():
 	parser = argparse.ArgumentParser(description="Analyzing the defecational behavior over the span of one year.")
@@ -20,9 +21,24 @@ def parse_input_arguments():
 	args = parser.parse_args()
 	return args
 
+def get_project_directory():
+	return os.path.dirname(os.path.realpath(__file__))
+
+def get_newest_filename():
+	return sorted(os.listdir("./data"))[-1]
+
+def check_file_exists(file):
+	return os.path.exists("./data/"+file)
 
 def main():
 	args = parse_input_arguments()
+	home_dir = get_project_directory()
+	if args.file:
+		print(check_file_exists(args.file))
+	else:
+		filename = get_newest_filename()
+		print(filename)
+
 
 
 if __name__ == "__main__":
