86424bd1e9056736c4b1f31364e9bcd19dd92329
nosd <nosd@zhaw.ch>
2020-05-02 12:22:58 +0200

    parsing lines into dictionaries

--- analyshit.py
+++ analyshit.py
@@ -15,7 +15,9 @@ Diagrams and Outputs:
 # General
 import argparse
 import os
+import sys
 import logging
+import re
 
 # Dash
 import dash
@@ -69,15 +71,15 @@ app.layout = html.Div([
 				figure=dict(
 					data=[
 						dict(
-							x=[1,2,3,4,5],
-							y=[4,2,3,4,2],
-							name='Test Graph'
-						),
-						dict(
-							x=[1,2,3,4,5],
-							y=[5,3,5,1,3],
-							name='lksjdf'
-						),
+							z=[
+								[1,2,3],
+								[3,2,1],
+								[2,2,2]
+							],
+							colorscale='Reds',
+							name='lksjdf',
+							type='heatmap'
+						)
 					],
 					layout=dict(
 						title='Calendar Heatmap'
@@ -100,33 +102,79 @@ def parse_input_arguments():
 
 def get_project_directory():
 	directory = os.path.dirname(os.path.realpath(__file__))
-	logging.debug("home directory: {}".format(directory))
 	return directory
 
-def get_newest_filename():
-	filename = sorted(os.listdir("./data"))[-1]
-	logging.debug("newest file: {}".format(filename))
+def get_newest_filename(data_dir):
+	filename = sorted(os.listdir(data_dir))[-1]
 	return filename
 
-def check_file_exists(file):
-	if os.path.exists("./data/"+file):
-		logging.debug("selected file exists")
-		return True
-	else:
-		logging.error("selected file does not exist!")
-		return False
+def check_file_exists(filepath):
+	return os.path.exists(filepath)
+
+
+def parse_line(filepath):
+	regex = re.compile("(\d{2}).(\d{2}).(\d{2})\s+([hnwd])\s+([gnw])\s*(ninja|glück|geiss|bier|neocolor)?")
+	with open(filepath,'r') as f:
+		for line in f:
+			match = regex.search(line)
+			if match:
+				entry = {}
+				entry["day"] = match.group(1)
+				entry["month"] = match.group(2)
+				entry["year"] = match.group(3)
+				entry["consistency"] = match.group(4)
+				entry["size"] = match.group(5)
+				entry["type"] = match.group(6)
+				yield entry
+			else:
+				logging.warning("could not parse line: {}".format(line))
+			
+
+
+
+def process_file(filepath):
+	logging.debug("processing file {}".format(filepath))
+
+	for entry in parse_line(filepath):
+		print(entry)
+
+
+
+
+
 
 def main():
-	logging.basicConfig(level=logging.DEBUG, datefmt="%H:%M:%S", format="[%(asctime)s] - %(levelname)s - %(message)s")
+	logging.basicConfig(level=logging.DEBUG, datefmt="%H:%M:%S", format="[%(asctime)s] - %(levelname)s - %(funcName)s ||| %(message)s")
 
 	args = parse_input_arguments()
 	home_dir = get_project_directory()
+	data_dir = os.path.join(home_dir,"data")
+	logging.debug("home_dir: {}".format(home_dir))
+	logging.debug("data_dir: {}".format(data_dir))
+
 	if args.file:
-		if not check_file_exists(args.file):
-			logging.critical("Leaving the application...")
+		# if not check_file_exists(args.file):
+		# 	logging.critical("Leaving the application...")
+		# 	sys.exit(-1)
+		# else:
+		# 	filename = args.file
+		# 	logging.debug("file {} exists!".format(filename))
+		filename = args.file
+		logging.debug("selected file: {}".format(filename))
+
 	else:
-		filename = get_newest_filename()
-		print(filename)
+		filename = get_newest_filename(data_dir)
+		logging.debug("getting newest file: {}".format(filename))
+
+	filepath = os.path.join(data_dir,filename)
+	logging.debug("absolute filepath: {}".format(filepath))
+	if not check_file_exists(filepath):
+		logging.error("File does not exist!")
+		logging.critical("Leaving the application...")
+		sys.exit(-1)
+	logging.debug("file exists!")
+
+	process_file(filepath)
 
 
 	print("\n\n")
